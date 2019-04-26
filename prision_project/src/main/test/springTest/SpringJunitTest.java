b4c841c79c353dda3c5ee8cb93601fd6c4a887fe
zqg <zqg@qq.com>
2019-04-26 22:35:59 +0800

    单元测试

--- prision_project/src/main/test/springTest/SpringJunitTest.java
+++ prision_project/src/main/test/springTest/SpringJunitTest.java
@@ -1,10 +1,11 @@
 package springTest;
 
 
+import club.zqg.prision.dao.PrisionAdminMapper;
 import club.zqg.prision.dao.PrisionNewsMapper;
-import club.zqg.prision.dao.PrisionPoliceMapper;
 import club.zqg.prision.dao.PrisionerMapper;
-import club.zqg.prision.domain.PrisionPolice;
+import club.zqg.prision.domain.PrisionAdmin;
+import club.zqg.prision.domain.PrisionAdminExample;
 import club.zqg.prision.domain.Prisioner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,25 +19,40 @@ import java.util.List;
 @ContextConfiguration("classpath:applicationContext.xml")
 public class SpringJunitTest {
 
+    //新闻测试
     @Autowired
     PrisionNewsMapper prisionNewsMapper;
+
     @Test
     public void testMapper() {
         System.out.println(prisionNewsMapper);
     }
 
+    // 犯人测试
     @Autowired
     PrisionerMapper prisionerMapper;
 
     @Test
-    public void testFindAllDetail(){
+    public void testFindAllDetail() {
         List<Prisioner> prisioners = prisionerMapper.selectDetailInfo();
         //System.out.println(prisioners);
-        for(Prisioner prisioner:prisioners){
+        for (Prisioner prisioner : prisioners) {
             System.out.println(prisioner);
             //System.out.println(prisioner.getDangerlevel().getDangerLevelName());
             //System.out.println(prisioner.getPrisionPolice().getPoliceName());
         }
     }
 
+    // 管理员测试
+    @Autowired
+    PrisionAdminMapper adminMapper;
+
+    @Test
+    public void testLogin() {
+        PrisionAdmin prisionAdmin = new PrisionAdmin();
+        prisionAdmin.setPrisionAdminName("zqg");
+        prisionAdmin.setPrisionAdminPwd("zqg");
+        PrisionAdmin prisionAdmin1 = adminMapper.selectSingle(prisionAdmin);
+        System.out.println(prisionAdmin1);
+    }
 }
