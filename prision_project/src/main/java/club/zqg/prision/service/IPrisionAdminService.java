58df6dc528acd3e615414f75752f512b183ed75c
zqg <zqg.qq.com>
2019-04-27 12:27:48 +0800

    管理员模块更新

--- prision_project/src/main/java/club/zqg/prision/service/IPrisionAdminService.java
+++ prision_project/src/main/java/club/zqg/prision/service/IPrisionAdminService.java
@@ -1,7 +1,9 @@
 package club.zqg.prision.service;
 
+import club.zqg.prision.domain.Dangerlevel;
 import club.zqg.prision.domain.PrisionAdmin;
 import club.zqg.prision.domain.PrisionPolice;
+import club.zqg.prision.domain.Prisioner;
 
 import java.util.List;
 
@@ -10,6 +12,10 @@ public interface IPrisionAdminService {
     // 管理员登陆校验
     PrisionAdmin login(PrisionAdmin admin);
 
+    //所属狱警
     List<PrisionPolice> findAllPrisionPolice();
 
+    List<Dangerlevel> findAllDangeLevel();
+
+    List<Prisioner> findQuickResult(String prisionName);
 }
--- prision_project/src/main/java/club/zqg/prision/service/serviceImp/PrisionAdminService.java
+++ prision_project/src/main/java/club/zqg/prision/service/serviceImp/PrisionAdminService.java
@@ -1,10 +1,14 @@
 package club.zqg.prision.service.serviceImp;
 
 
+import club.zqg.prision.dao.DangerlevelMapper;
 import club.zqg.prision.dao.PrisionAdminMapper;
 import club.zqg.prision.dao.PrisionPoliceMapper;
+import club.zqg.prision.dao.PrisionerMapper;
+import club.zqg.prision.domain.Dangerlevel;
 import club.zqg.prision.domain.PrisionAdmin;
 import club.zqg.prision.domain.PrisionPolice;
+import club.zqg.prision.domain.Prisioner;
 import club.zqg.prision.service.IPrisionAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,18 +18,38 @@ import java.util.List;
 @Service
 public class PrisionAdminService implements IPrisionAdminService {
 
+
     @Autowired
     PrisionAdminMapper adminMapper; // 管理员Mapper
 
     @Autowired
-    PrisionPoliceMapper policeMapper;
+    PrisionPoliceMapper policeMapper; // 狱警信息
+
+    @Autowired
+    DangerlevelMapper dangerlevelMapper; // 危险等级
+
+    @Autowired
+    PrisionerMapper prisionerMapper;
+
     // 登陆校验
     @Override
     public PrisionAdmin login(PrisionAdmin admin) {
         return adminMapper.selectSingle(admin);
     }
-    // 狱警信息
+    // 所属狱警信息
     public List<PrisionPolice> findAllPrisionPolice(){
         return policeMapper.selectByExample(null);
+    }
+
+    // 所属危险等级
+    @Override
+    public List<Dangerlevel> findAllDangeLevel() {
+        return dangerlevelMapper.selectByExample(null);
+    }
+
+    // 快捷查询
+    @Override
+    public List<Prisioner> findQuickResult(String prisionName) {
+        return prisionerMapper.findQuickResult(prisionName) ;
     }
 }
