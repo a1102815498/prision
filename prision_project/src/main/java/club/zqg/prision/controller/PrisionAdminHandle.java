62b324ab0fa50a464bb2209b226dcef0cac3f83c
zqg <zqg.qq.com>
2019-04-27 12:31:09 +0800

    管理员模块更新

--- prision_project/src/main/java/club/zqg/prision/controller/PrisionAdminHandle.java
+++ prision_project/src/main/java/club/zqg/prision/controller/PrisionAdminHandle.java
@@ -2,14 +2,18 @@ package club.zqg.prision.controller;
 
 
 import club.zqg.prision.domain.PrisionAdmin;
+import club.zqg.prision.domain.Prisioner;
 import club.zqg.prision.service.IPrisionAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
 
 // 监狱管理员[负责犯人信息录入与查询，狱警信息录入与查询，新闻发布等]
 
@@ -34,11 +38,24 @@ public class PrisionAdminHandle {
         if(adminService.login(admin)!=null){
             model.addAttribute("adminName",admin.getPrisionAdminName());
             model.addAttribute("AllprisionerPolice",adminService.findAllPrisionPolice());
+            model.addAttribute("AllDangerLevel",adminService.findAllDangeLevel());
             return "/prision/user";
         }else{
             model.addAttribute("msg","用户名或密码错误");
             return "/prision/login";
         }
+    }
+
+    // 快捷查询
+    @ResponseBody
+    @RequestMapping("/quickQuery")
+    public List<Prisioner> quickQuery(@RequestParam(value = "name")String prisionerName){
+        System.out.println("接受的参数"+prisionerName);
+        List<Prisioner> quickResult = adminService.findQuickResult(prisionerName);
+        for (Prisioner prisioner:quickResult){
+            System.out.println(prisioner.getPrisionPolice().getPoliceName());
+        }
+        return quickResult;
     }
 
 
